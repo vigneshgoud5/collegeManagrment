04ccc7d15231f9a39a2fee10b19371e7ac3b5222
vigneshgoud5 <vigneshgoud1337.com>
2025-11-19 22:31:15 +0530

    fix: resolved client & server test failures

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -10,6 +10,12 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
+    pool: 'forks',
+    poolOptions: {
+      forks: {
+        singleFork: true,
+      },
+    },
   },
 });
 
